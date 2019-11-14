27612df60f6402774f0c6b76ac7a6380acb1bfe2
afterburner <rockey1024@icloud.com>
2019-11-14 17:31:50 +0800

    Showed image

--- UI/Views/ImageInfo/ImageInfoView.xaml.cs
+++ UI/Views/ImageInfo/ImageInfoView.xaml.cs
@@ -13,9 +13,8 @@ namespace UI.Views.ImageInfo
 
         private void ImageInfoView_OnLoaded(object sender, RoutedEventArgs e)
         {
-//            SmartWindowControlWpf.HalconWindow.SetLut("twelve");
+            SmartWindowControlWpf.HalconWindow.SetLut("twelve");
             var dataContext = DataContext as ImageInfoViewModel;
-            SmartWindowControlWpf.HalconWindow.DispImage(dataContext.Image);
         }
     }
 }
